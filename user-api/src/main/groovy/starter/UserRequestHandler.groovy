7d74dc4f2589136cd67b43eaebe5ee65243143be
Vladimir Orany <vladimir@orany.cz>
2018-12-21 12:07:55 +0100

    initializing the handler with starter package

--- user-api/src/main/groovy/starter/UserRequestHandler.groovy
+++ user-api/src/main/groovy/starter/UserRequestHandler.groovy
@@ -3,14 +3,17 @@ package starter
 import com.agorapulse.micronaut.agp.ApiGatewayProxyHandler
 import groovy.transform.CompileStatic
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
 
 @CompileStatic
 class UserRequestHandler extends ApiGatewayProxyHandler {
 
-    @Override
-    protected void doWithApplicationContext(ApplicationContext applicationContext) {
-        // Required for GORM package scanning
-        applicationContext.environment.addPackage('starter')
+    protected ApplicationContext buildApplicationContext() {
+        return ApplicationContext.build().packages('starter').environments(
+                API_GATEWAY_PROXY_ENVIRONMENT,
+                Environment.AMAZON_EC2,
+                Environment.CLOUD
+        ).build();
     }
 
 }
